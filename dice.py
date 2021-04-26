8f9ee9ec33514e6a6d50fdb71b14eae97b673964
Roshan Abeysekera <roshanabey2@gmail.com>
2021-04-26 14:52:55 +0100

    Added the Module tutorial and exercise questions

---
+++ dice.py
@@ -0,0 +1,6 @@
+import random
+
+
+def throw_dice():
+    value = random.randrange(1, 7)
+    return value
---
+++ throwing.py
@@ -0,0 +1,7 @@
+import dice
+
+first_throw = dice.throw_dice()
+second_throw = dice.throw_dice()
+
+total = first_throw + second_throw
+print(total)
